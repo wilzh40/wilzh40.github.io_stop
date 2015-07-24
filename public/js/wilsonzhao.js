71bffe41a5686e0d0025c26fd5ba1c18351c04d3
wilzh40 <wilzh40@gmail.com>
2015-07-23 20:11:03 -0700

    various formatting changes

--- public/js/wilsonzhao.js
+++ public/js/wilsonzhao.js
@@ -2,7 +2,6 @@ jQuery(window).on('load', function(){
     var $ = jQuery;     
     var $container = $('#mason');
         $container.masonry({
-                      columnWidth:10, 
                       gutterWidth: 0,
                       itemSelector: '.project'
                 });
